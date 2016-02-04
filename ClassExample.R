207630fe981893f8953d68c2c40579728eeece9c
ScottSolomon66 <scott.solomon66@gmail.com>
2016-02-04 14:08:11 -0600

    new branch
    new branch

--- ClassExample.R
+++ ClassExample.R
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 print("made another change")
+=======
+
+>>>>>>> parent of 73e062f... made a change
 
 print("Hello World")
 
@@ -7,3 +11,5 @@ print("A new line")
 
 
 print("A third line")
+
+print("I made a new branch")
